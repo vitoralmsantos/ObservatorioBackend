9246888556568e91e66b5c6494dab8505a4f3199
raquelfontenelleferreira <raquelfontenelle@gmail.com>
2020-02-17 15:36:29 -0300

    RAQUEL - DESI

---
+++ ObservatorioBack/Models/RespostaExt.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ObservatorioBack.Models
+{
+    public  partial class Resposta
+    { 
+
+    }  
+}
